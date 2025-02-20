dcfb0e86d427d4747a42b7a72aff339b37afb01f
DMYTRO YURCHYSHYN <gost0371337@gmail.com>
2025-02-20 13:27:30 +0200

    Add schemas for data validation and serialization

---
+++ mcdonalds_scraper/api/schemas/mcdonalds.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+class MenuItemSchema(BaseModel):
+    name: str
+    description: str | None
+    calories: int | None
+    fats: float | None
+    carbs: float | None
+    proteins: float | None
+    unsaturated_fats: float | None
+    sugar: float | None
+    salt: float | None
+    portion: int | None
